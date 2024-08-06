39afa4b5e827dc43175d8aee5255fa8986685a0e
yuta asakura <yuta.asakura.se@gmail.com>
2024-08-06 14:20:40 +0900

    finished excercise

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,24 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # READ ALL
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # READ ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #  # DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
